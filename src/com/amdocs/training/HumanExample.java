96469ef0f13e507ddcae368e733f3dfbe1815ef0
ameyanaik <ameya.naik@destinationqa.com>
2019-09-17 16:19:35 +0530

    Committing from Intellij

--- src/com/amdocs/training/HumanExample.java
+++ src/com/amdocs/training/HumanExample.java
@@ -8,7 +8,7 @@ public class HumanExample {
         Human h3 = new Human();
         h3.setFirstname("Tom");
         h3.talk();
-
+        h3.setLastname("Last Name");
         //h3.count = 0;
 
         Human.count = 5;
